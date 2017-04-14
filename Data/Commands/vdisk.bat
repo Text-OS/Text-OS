139173dddb86c5b98d273fc4d3bdb72b32a403f1
Rasmus <rasmusbamse@hotmail.com>
2017-04-14 13:00:42 +0200

    make the drive letters upper case

--- Data/Commands/vdisk.bat
+++ Data/Commands/vdisk.bat
@@ -1,32 +1,32 @@
 cd VirtualDrives
 echo Enter letter: 
 choice /c:abcdefghijklmnopqrstuvwxyz /n
-if %errorlevel% == 1 set TextOS.MountedDrive=a
-if %errorlevel% == 2 set TextOS.MountedDrive=b
-if %errorlevel% == 3 set TextOS.MountedDrive=c
-if %errorlevel% == 4 set TextOS.MountedDrive=d
-if %errorlevel% == 5 set TextOS.MountedDrive=e
-if %errorlevel% == 6 set TextOS.MountedDrive=f
-if %errorlevel% == 7 set TextOS.MountedDrive=g
-if %errorlevel% == 8 set TextOS.MountedDrive=h
-if %errorlevel% == 9 set TextOS.MountedDrive=i
-if %errorlevel% == 10 set TextOS.MountedDrive=j
-if %errorlevel% == 11 set TextOS.MountedDrive=k
-if %errorlevel% == 12 set TextOS.MountedDrive=l
-if %errorlevel% == 13 set TextOS.MountedDrive=m
-if %errorlevel% == 14 set TextOS.MountedDrive=n
-if %errorlevel% == 15 set TextOS.MountedDrive=o
-if %errorlevel% == 16 set TextOS.MountedDrive=p
-if %errorlevel% == 17 set TextOS.MountedDrive=q
-if %errorlevel% == 18 set TextOS.MountedDrive=r
-if %errorlevel% == 19 set TextOS.MountedDrive=s
-if %errorlevel% == 20 set TextOS.MountedDrive=t
-if %errorlevel% == 21 set TextOS.MountedDrive=u
-if %errorlevel% == 22 set TextOS.MountedDrive=v
-if %errorlevel% == 23 set TextOS.MountedDrive=w
-if %errorlevel% == 24 set TextOS.MountedDrive=x
-if %errorlevel% == 25 set TextOS.MountedDrive=y
-if %errorlevel% == 26 set TextOS.MountedDrive=z
+if %errorlevel% == 1 set TextOS.MountedDrive=A
+if %errorlevel% == 2 set TextOS.MountedDrive=B
+if %errorlevel% == 3 set TextOS.MountedDrive=C
+if %errorlevel% == 4 set TextOS.MountedDrive=D
+if %errorlevel% == 5 set TextOS.MountedDrive=E
+if %errorlevel% == 6 set TextOS.MountedDrive=F
+if %errorlevel% == 7 set TextOS.MountedDrive=G
+if %errorlevel% == 8 set TextOS.MountedDrive=H
+if %errorlevel% == 9 set TextOS.MountedDrive=I
+if %errorlevel% == 10 set TextOS.MountedDrive=J
+if %errorlevel% == 11 set TextOS.MountedDrive=K
+if %errorlevel% == 12 set TextOS.MountedDrive=L
+if %errorlevel% == 13 set TextOS.MountedDrive=M
+if %errorlevel% == 14 set TextOS.MountedDrive=N
+if %errorlevel% == 15 set TextOS.MountedDrive=O
+if %errorlevel% == 16 set TextOS.MountedDrive=P
+if %errorlevel% == 17 set TextOS.MountedDrive=Q
+if %errorlevel% == 18 set TextOS.MountedDrive=R
+if %errorlevel% == 19 set TextOS.MountedDrive=S
+if %errorlevel% == 20 set TextOS.MountedDrive=T
+if %errorlevel% == 21 set TextOS.MountedDrive=U
+if %errorlevel% == 22 set TextOS.MountedDrive=V
+if %errorlevel% == 23 set TextOS.MountedDrive=W
+if %errorlevel% == 24 set TextOS.MountedDrive=X
+if %errorlevel% == 25 set TextOS.MountedDrive=Y
+if %errorlevel% == 26 set TextOS.MountedDrive=Z
 if not exist !TextOS.MountedDrive! mkdir !TextOS.MountedDrive!
 cd !TextOS.MountedDrive!
 echo Mounted.
